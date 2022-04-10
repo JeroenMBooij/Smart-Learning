1aa8fc4d8c9579d01499100a58ec43d98f0baf57
JeroenMBooij <49719495+JeroenMBooij@users.noreply.github.com>
2022-04-10 12:51:00 +0200

    Transcribe client

---
+++ API/TeacherDidac.Infrastructure/Transcribe/TranscribeResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherDidac.Infrastructure.Transcribe
+{
+    public class TranscribeResult
+    {
+        public string[] Predictions { get; set; }
+        public decimal[] Probabilities { get; set; }
+    }
+}
